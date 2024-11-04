8ef9652d4fe52e0599f1e03e8f7f29992a95c29e
Kenneth Aladi <kendevops@gmail.com>
2024-11-04 15:03:27 +0100

    feat 🚀 - implemented the context Api for the application

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 const TopBar = () => {
   return (
-    <div className="w-full h-[101px] border-b-1 border-b-light bg-white">
+    <div className="w-full h-[101px] border-b border-b-light bg-white">
       <div>
         <h1>Topbar</h1>
       </div>
